37cb9a645cb6ebebc780d4f9b2ce77d6f9f75cdb
Muhammad Abid <abidroid1@gmail.com>
2024-06-15 17:51:01 +0500

    lazy grid practice

--- app/src/main/java/com/example/listgridinjetpackcompose/DoctorsListScreen.kt
+++ app/src/main/java/com/example/listgridinjetpackcompose/DoctorsListScreen.kt
@@ -1,12 +1,28 @@
 package com.example.listgridinjetpackcompose
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
@@ -15,12 +31,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DoctorsListScreen() {
+
+    val doctorsList = retrieveDoctors()
+    val myContext = LocalContext.current
 
     Scaffold(
         topBar = {
@@ -31,15 +53,64 @@ fun DoctorsListScreen() {
         },
 
         content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(it)
-                    .background(Color.White),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text("Yes")
+
+            LazyColumn(
+                modifier = Modifier.padding(it),
+
+                ) {
+                items(
+                    count = doctorsList.count(),
+                    itemContent = { index ->
+                        val doctor = doctorsList[index]
+
+                        Card(
+
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(7.dp),
+
+                            onClick = {
+                                Toast.makeText(myContext, "You clicked ${doctor.name}", Toast.LENGTH_SHORT)
+
+                            },
+                            content = {
+
+                                Row(
+                                    modifier = Modifier
+                                        .padding(7.dp)
+                                        .fillMaxWidth()
+                                ) {
+                                    Row(
+                                        //modifier = Modifier.fillMaxWidth()
+                                    ) {
+
+                                        Image(
+                                            painter = painterResource(id = doctor.image),
+                                            contentDescription = "Doctor Image",
+
+                                            )
+
+                                        Spacer(modifier = Modifier.padding(8.dp))
+                                        Column(
+                                            verticalArrangement = Arrangement.Center
+                                        ) {
+                                            Text(doctor.name, fontSize = 30.sp)
+                                            Spacer(modifier = Modifier.padding(8.dp))
+                                            Text(doctor.spe)
+                                        }
+                                    }
+
+                                    Button(onClick = { /*TODO*/ }) {
+                                        androidx.compose.material3.Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Favorite")
+                                    }
+
+
+                                }
+                            }
+                        )
+
+                    }
+                )
             }
         }
     )
--- app/src/main/java/com/example/listgridinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/listgridinjetpackcompose/MainActivity.kt
@@ -18,30 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ListGridInJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            DoctorsListScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ListGridInJetpackComposeTheme {
-        Greeting("Android")
-    }
-}
