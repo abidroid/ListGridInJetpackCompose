746d1c29e5815061915fc024a0cd241662111f3f
Muhammad Abid <abidroid1@gmail.com>
2024-06-15 14:23:24 +0500

    main screen design

---
+++ app/src/main/java/com/example/listgridinjetpackcompose/DoctorsListScreen.kt
@@ -0,0 +1,55 @@
+package com.example.listgridinjetpackcompose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DoctorsListScreen() {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+
+                title = { Text("Doctors List", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Blue)
+
+
+            )
+        },
+
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+                    .background(Color.White),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text("Yes")
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun DoctorsListScreenPreview() {
+    DoctorsListScreen()
+}
