7a44ca1007489b80d15b91661bfac72f501801d2
Muhammad Abid <abidroid1@gmail.com>
2024-06-16 22:42:22 +0500

    Lazy Row created

---
+++ app/src/main/java/com/example/listgridinjetpackcompose/DoctorListLazyRow.kt
@@ -0,0 +1,77 @@
+package com.example.listgridinjetpackcompose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DoctorListLazyRow(navController: NavController) {
+
+    val doctors = retrieveDoctors()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Doctors") },
+            )
+        },
+
+        content = {
+
+            LazyRow(
+                modifier = Modifier.padding(it),
+            ) {
+                items(doctors.count(),
+                    itemContent = { index ->
+                        val doctor = doctors[index]
+                        Card(
+
+                            onClick = {
+
+                            },
+
+                            modifier = Modifier.padding(10.dp)
+
+                        ) {
+                            Column(){
+                                Image(painter = painterResource(id = doctor.image), contentDescription = doctor.name)
+                                Spacer(modifier = Modifier.height(10.dp))
+                                Text("Name: ${doctor.name}")
+                                Spacer(modifier = Modifier.height(10.dp))
+                                Text(text = "Speciality: ${doctor.spe}")
+                                Spacer(modifier = Modifier.height(10.dp))
+                                Text(text = "Gender: ${doctor.gender}")
+                                Spacer(modifier = Modifier.height(10.dp))
+
+                                Button(onClick = { /*TODO*/ }) {
+                                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "Favorite")
+                                }
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    )
+}
