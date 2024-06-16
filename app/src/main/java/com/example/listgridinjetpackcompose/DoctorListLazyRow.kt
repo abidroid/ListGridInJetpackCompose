d90711dea2c6f9a11cbb466b764efe5a52393567
Muhammad Abid <abidroid1@gmail.com>
2024-06-16 23:08:26 +0500

    lazy row designed

--- app/src/main/java/com/example/listgridinjetpackcompose/DoctorListLazyRow.kt
+++ app/src/main/java/com/example/listgridinjetpackcompose/DoctorListLazyRow.kt
@@ -1,10 +1,12 @@
 package com.example.listgridinjetpackcompose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
@@ -18,8 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -51,18 +56,35 @@ fun DoctorListLazyRow(navController: NavController) {
 
                             },
 
-                            modifier = Modifier.padding(10.dp)
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .height(250.dp)
 
                         ) {
-                            Column(){
-                                Image(painter = painterResource(id = doctor.image), contentDescription = doctor.name)
-                                Spacer(modifier = Modifier.height(10.dp))
-                                Text("Name: ${doctor.name}")
-                                Spacer(modifier = Modifier.height(10.dp))
-                                Text(text = "Speciality: ${doctor.spe}")
-                                Spacer(modifier = Modifier.height(10.dp))
-                                Text(text = "Gender: ${doctor.gender}")
-                                Spacer(modifier = Modifier.height(10.dp))
+                            Column(
+                                verticalArrangement = Arrangement.SpaceBetween,
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.padding(8.dp)
+
+                            ) {
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier.weight(1f)
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = doctor.image), contentDescription = doctor.name,
+                                        contentScale = ContentScale.FillBounds,
+                                        modifier = Modifier.size(80.dp, 80.dp)
+
+                                    )
+                                    Spacer(modifier = Modifier.height(10.dp))
+                                    Text("Name: ${doctor.name}")
+                                    Spacer(modifier = Modifier.height(10.dp))
+                                    Text(text = "Speciality: ${doctor.spe}")
+                                    Spacer(modifier = Modifier.height(10.dp))
+                                    Text(text = "Gender: ${doctor.gender}")
+                                    Spacer(modifier = Modifier.height(10.dp))
+                                }
 
                                 Button(onClick = { /*TODO*/ }) {
                                     Icon(imageVector = Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "Favorite")
--- app/src/main/java/com/example/listgridinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/listgridinjetpackcompose/MainActivity.kt
@@ -4,19 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.listgridinjetpackcompose.ui.theme.ListGridInJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,9 +29,9 @@ fun MyNavigation() {
 
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "DoctorsListScreen") {
-        composable("DoctorsListScreen") {
-            DoctorsListScreen(navController)
+    NavHost(navController = navController, startDestination = "DoctorListLazyRow") {
+        composable("DoctorListLazyRow") {
+            DoctorListLazyRow(navController)
         }
         composable("DoctorDetailScreen/{doctorName}",
             arguments = listOf(
