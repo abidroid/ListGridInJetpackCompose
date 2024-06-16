90af4290b37bbd881f6bc2b110310939155d3429
Muhammad Abid <abidroid1@gmail.com>
2024-06-16 14:07:57 +0500

    navigation done to doctor detail screen

--- app/src/main/java/com/example/listgridinjetpackcompose/DoctorDetailScreen.kt
+++ app/src/main/java/com/example/listgridinjetpackcompose/DoctorDetailScreen.kt
@@ -1,8 +1,13 @@
 package com.example.listgridinjetpackcompose
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,12 +17,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DoctorDetailScreen(navController: NavController, doctorName: String) {
+
+    val doctorsList = retrieveDoctors()
+
+    val doctor = doctorsList.find { doctor -> doctor.name == doctorName }
 
     Scaffold(
         topBar = {
@@ -35,10 +47,28 @@ fun DoctorDetailScreen(navController: NavController, doctorName: String) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(it)
+                    .padding(it),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                Text("Name: $doctorName")
+                if (doctor == null) {
+                    Text("Doctor not found")
+                    return@Column
+                }
+
+                if( doctor.image != null ){
+                    Image(painter = painterResource(id = doctor.image), contentDescription = "Doctor Image",
+                        modifier = Modifier.size(300.dp)
+                        )
+
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Text("Name: ${doctor?.name}")
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Gender: ${doctor?.gender}")
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Specialization: ${doctor?.spe}")
             }
         }
 
