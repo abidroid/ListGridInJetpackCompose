b7bc18cf77f540b6fa12011231538a6cefe5ef6d
Muhammad Abid <abidroid1@gmail.com>
2024-06-17 22:06:29 +0500

    design improvements

--- app/src/main/java/com/example/listgridinjetpackcompose/DoctorListLazyVGrid.kt
+++ app/src/main/java/com/example/listgridinjetpackcompose/DoctorListLazyVGrid.kt
@@ -6,15 +6,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,12 +56,37 @@ fun DoctorListLazyVGrid(navController: NavController) {
 
                         val doctor = doctors[index]
 
-                        Card {
-                            Image(painter = painterResource(id = doctor.image), contentDescription = doctor.name)
-                            Text(doctor.name, fontSize = 20.sp)
-                            Spacer(modifier = androidx.compose.ui.Modifier.padding(4.dp))
-                            Text(doctor.spe)
-                        }
+                        Card(
+                            modifier = androidx.compose.ui.Modifier.height(300.dp),
+                            onClick = {
+                                navController.navigate("DoctorDetailScreen/${doctor.name}")
+                            },
+
+                            content = {
+                                Column(
+                                    modifier = androidx.compose.ui.Modifier
+                                        .padding(8.dp)
+                                        .fillMaxWidth(),
+                                    verticalArrangement = Arrangement.SpaceBetween,
+                                    horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = doctor.image), contentDescription = doctor.name,
+
+                                        modifier = androidx.compose.ui.Modifier
+                                            .size(150.dp)
+                                            .clip(RoundedCornerShape(8.dp)),
+                                        contentScale = ContentScale.FillBounds,
+
+                                        )
+                                    Spacer(modifier = androidx.compose.ui.Modifier.padding(4.dp))
+
+                                    Text(doctor.name, fontSize = 20.sp)
+                                    Spacer(modifier = androidx.compose.ui.Modifier.padding(4.dp))
+                                    Text(doctor.spe)
+                                }
+                            }
+                        )
                     }
                 }
             }
